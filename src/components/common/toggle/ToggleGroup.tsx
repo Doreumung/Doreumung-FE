015a879cecbe8e9e203bc5fd015433f6a0f48243
Seyoon Jeong <seyoonagain@gmail.com>
2024-12-27 15:39:46 +0900

    chore: Toggle 타입 업데이트

--- src/components/common/toggle/ToggleGroup.tsx
+++ src/components/common/toggle/ToggleGroup.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { toggleStyles } from './toggleStyles';
 import { THEMES } from './constants';
 import { ToggleGroupProps } from './types';
@@ -6,7 +6,7 @@ import { ToggleGroupProps } from './types';
 // 일정 생성 시, 테마 및 식사 여부를 선택하기 위한 ToggleGroup
 // 배열에 담긴 다수의 선택지를 items로 통해 전달받아 여러 Toggle 버튼 렌더링
 // onChange는 ToggleGroup을 호출한 컴포넌트 내에서 정의된 함수로, 토글에 의해 선택된 아이템들의 인덱스를 담은 배열을 전달
-const ToggleGroup = ({ items = THEMES, onChange }: ToggleGroupProps) => {
+const ToggleGroup: React.FC<ToggleGroupProps> = ({ items = THEMES, onChange }) => {
   const [activeToggles, setActiveToggle] = useState<number[]>([]);
 
   const handleToggle = (index: number) => {
