2b4213cd02a6c4da5c1c3f6ea6fcc8438f761299
Seyoon Jeong <seyoonagain@gmail.com>
2024-12-29 22:02:31 +0900

    feat: Logo 반응형 구현에 따른 모션 업데이트

--- src/components/common/header/logo/LogoImage.tsx
+++ src/components/common/header/logo/LogoImage.tsx
@@ -2,35 +2,60 @@
 
 import Dolmung from '@public/images/dolmung.svg';
 import Image from 'next/image';
-import { motion, useScroll, useTransform } from 'motion/react';
+import { motion, MotionValue, useMotionValue, useScroll, useTransform } from 'motion/react';
 import { LogoTextAndImageProps } from './types';
-import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
-import { INPUT_RANGE } from './constants';
+import { INPUT_RANGE, LOGO_STYLES } from '../constants';
+import useIsMobile from '@/hooks/useIsMobile';
+import { useEffect } from 'react';
 
 const LogoImage: React.FC<LogoTextAndImageProps> = ({ variant }) => {
+  const isMobile = useIsMobile();
   const { scrollYProgress } = useScroll();
+  const deviceType = isMobile ? 'mobile' : 'web';
 
-  const motionWidth = useTransform(scrollYProgress, INPUT_RANGE, ['56px', '48px', '48px']);
-  const motionLeft = useTransform(scrollYProgress, INPUT_RANGE, ['217px', '160px', '160px']);
-  const motionBottom = useTransform(scrollYProgress, INPUT_RANGE, ['55px', '12px', '12px']);
-  const motionTransform = useTransform(scrollYProgress, INPUT_RANGE, [
-    'rotate(0)',
-    'rotate(360deg)',
-    'rotate(360deg)',
-  ]);
+  const motionStyles = LOGO_STYLES.image.motion[deviceType];
+  const defaultStyles = LOGO_STYLES.image.default[deviceType];
 
-  const width = variant === 'motion' ? motionWidth : '48px';
-  const left = variant === 'motion' ? motionLeft : '160px';
-  const bottom = variant === 'motion' ? motionBottom : '12px';
-  const transform = variant === 'motion' ? motionTransform : undefined;
+  const createMotionValue = (
+    key: keyof typeof motionStyles,
+  ): [MotionValue<number>, number[], string[]] => [
+    scrollYProgress,
+    INPUT_RANGE,
+    [motionStyles[key], defaultStyles[key], defaultStyles[key]],
+  ];
+
+  const motionWidth = useTransform(...createMotionValue('width'));
+  const motionLeft = useTransform(...createMotionValue('left'));
+  const motionBottom = useTransform(...createMotionValue('bottom'));
+  const motionTransform = useTransform(...createMotionValue('transform'));
+  const defaultWidth = useMotionValue(defaultStyles.width);
+  const defaultLeft = useMotionValue(defaultStyles.left);
+  const defaultBottom = useMotionValue(defaultStyles.bottom);
+  const defaultTransform = useMotionValue(defaultStyles.transform);
+
+  const position = useMotionValue(LOGO_STYLES.position);
+  const width = variant === 'motion' ? motionWidth : defaultWidth;
+  const left = variant === 'motion' ? motionLeft : defaultLeft;
+  const bottom = variant === 'motion' ? motionBottom : defaultBottom;
+  const transform = variant === 'motion' ? motionTransform : defaultTransform;
+
+  const motionStyle = { position, width, bottom, left, transform };
+
+  useEffect(() => {
+    motionWidth.set(motionStyles.width);
+    motionLeft.set(motionStyles.left);
+    motionBottom.set(motionStyles.bottom);
+    motionTransform.set(motionStyles.transform);
+    defaultWidth.set(defaultStyles.width);
+    defaultLeft.set(defaultStyles.left);
+    defaultBottom.set(defaultStyles.bottom);
+    defaultTransform.set(defaultStyles.transform);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deviceType]);
 
   return (
     <>
-      <motion.div
-        className={twMerge(clsx(variant === 'default' && 'w-12 bottom-3 left-40'), 'absolute')}
-        style={{ width, left, bottom, transform }}
-      >
+      <motion.div style={motionStyle}>
         <Image src={Dolmung} alt="dolmung" />
       </motion.div>
     </>
--- src/components/common/header/logo/LogoText.tsx
+++ src/components/common/header/logo/LogoText.tsx
@@ -3,27 +3,51 @@
 import DoreumungLogo from '@public/images/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
-import { motion, useScroll, useTransform } from 'motion/react';
+import { motion, MotionValue, useMotionValue, useScroll, useTransform } from 'motion/react';
 import { LogoTextAndImageProps } from './types';
-import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
-import { INPUT_RANGE } from './constants';
+import { INPUT_RANGE, LOGO_STYLES } from '../constants';
+import useIsMobile from '@/hooks/useIsMobile';
+import { useEffect } from 'react';
 
 const LogoText: React.FC<LogoTextAndImageProps> = ({ variant }) => {
+  const isMobile = useIsMobile();
   const { scrollYProgress } = useScroll();
+  const deviceType = isMobile ? 'mobile' : 'web';
 
-  const motionWidth = useTransform(scrollYProgress, INPUT_RANGE, ['172px', '128px', '128px']);
-  const motionMarginLeft = useTransform(scrollYProgress, INPUT_RANGE, ['56px', '32px', '32px']);
+  const motionStyles = LOGO_STYLES.text.motion[deviceType];
+  const defaultStyles = LOGO_STYLES.text.default[deviceType];
 
-  const width = variant === 'motion' ? motionWidth : '128px';
-  const marginLeft = variant === 'motion' ? motionMarginLeft : '32px';
+  const createMotionValue = (
+    key: keyof typeof motionStyles,
+  ): [MotionValue<number>, number[], string[]] => [
+    scrollYProgress,
+    INPUT_RANGE,
+    [motionStyles[key], defaultStyles[key], defaultStyles[key]],
+  ];
+
+  const motionWidth = useTransform(...createMotionValue('width'));
+  const motionMarginLeft = useTransform(...createMotionValue('marginLeft'));
+  const defaultWidth = useMotionValue(defaultStyles.width);
+  const defaultMarginLeft = useMotionValue(defaultStyles.marginLeft);
+
+  const position = useMotionValue(LOGO_STYLES.position);
+  const bottom = useMotionValue(LOGO_STYLES.text.bottom);
+  const width = variant === 'motion' ? motionWidth : defaultWidth;
+  const marginLeft = variant === 'motion' ? motionMarginLeft : defaultMarginLeft;
+
+  const motionStyle = { position, bottom, width, marginLeft };
+
+  useEffect(() => {
+    motionWidth.set(motionStyles.width);
+    motionMarginLeft.set(motionStyles.marginLeft);
+    defaultWidth.set(defaultStyles.width);
+    defaultMarginLeft.set(defaultStyles.marginLeft);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deviceType]);
 
   return (
     <>
-      <motion.div
-        className={twMerge(clsx(variant === 'default' && 'w-32 ml-8'), 'absolute bottom-2')}
-        style={{ width, marginLeft }}
-      >
+      <motion.div style={motionStyle}>
         <Link href="/">
           <Image src={DoreumungLogo} alt="logo" />
         </Link>
