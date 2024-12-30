d5afe100c38dfa976507699c42207890fc1b7db9
Kwonyeojiny <kangagi0009@naver.com>
2024-12-31 00:14:38 +0900

    refactor: useRef로 input 컴포넌트의 포워딩 기능 추가

--- src/components/common/inputs/Input.tsx
+++ src/components/common/inputs/Input.tsx
@@ -1,56 +1,52 @@
+'use client';
+
 import clsx from 'clsx';
-import React, { useState } from 'react';
+import React, { useState, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { inputStyles, inputWrapperStyles, passwordStyle } from './inputStyles';
 import { InputProps } from './types';
 import { Eye, EyeOff } from 'lucide-react';
 
-const Input: React.FC<InputProps> = ({
-  id,
-  label,
-  type,
-  placeholder,
-  error,
-  variant,
-  width,
-  className,
-  ...props
-}) => {
-  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, type, placeholder, error, variant, width, className, ...props }, ref) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
 
-  const isPasswordInput = type === 'password';
-  const inputType = isPasswordInput && isPasswordVisible ? 'text' : type;
+    const isPasswordInput = type === 'password';
+    const inputType = isPasswordInput && isPasswordVisible ? 'text' : type;
 
-  return (
-    <div className={clsx(inputWrapperStyles({ variant, width }))}>
-      {label && (
-        <label
-          htmlFor={id}
-          className={clsx('pl-3 text-logo ', variant === 'signin' && 'absolute top-1')}
-        >
-          {label}
-        </label>
-      )}
-      <input
-        id={id}
-        type={inputType}
-        placeholder={placeholder}
-        className={twMerge(clsx(inputStyles({ variant }), isPasswordInput && 'pr-12'), className)}
-        {...props}
-      />
-      {isPasswordInput && (
-        <button
-          type="button"
-          className={clsx(passwordStyle({ variant }))}
-          onClick={() => setIsPasswordVisible(prev => !prev)}
-        >
-          {isPasswordVisible ? <EyeOff className=" h-5 w-5" /> : <Eye className="h-5 w-5" />}
-        </button>
-      )}
-      {/* [임시 코드] 사용할 때 맞춰 수정해야 함 */}
-      {error && <p className="text-sm text-logo">{error}</p>}
-    </div>
-  );
-};
+    return (
+      <div className={clsx(inputWrapperStyles({ variant, width }))}>
+        {label && (
+          <label
+            htmlFor={id}
+            className={clsx('pl-3 text-logo ', variant === 'signin' && 'absolute top-1')}
+          >
+            {label}
+          </label>
+        )}
+        <input
+          id={id}
+          type={inputType}
+          placeholder={placeholder}
+          className={twMerge(clsx(inputStyles({ variant }), isPasswordInput && 'pr-12'), className)}
+          ref={ref}
+          {...props}
+        />
+        {isPasswordInput && (
+          <button
+            type="button"
+            className={clsx(passwordStyle({ variant }))}
+            onClick={() => setIsPasswordVisible(prev => !prev)}
+          >
+            {isPasswordVisible ? <EyeOff className=" h-5 w-5" /> : <Eye className="h-5 w-5" />}
+          </button>
+        )}
+        {error && <p className="text-sm text-logo">{error}</p>}
+      </div>
+    );
+  },
+);
+
+Input.displayName = 'Input';
 
 export default Input;
