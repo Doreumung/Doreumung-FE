5d758cdf28bb7d64d4ddd1d187ddc02681e6762e
Seyoon Jeong <seyoonagain@gmail.com>
2024-12-31 03:09:20 +0900

    feat: 여행 후기 작성 페이지 업데이트
    - 제출 버튼 추가
    - 제출 시 등록 확인을 위한 레이어 팝업 트리거
    - 제목 input에 ref 등록

--- src/app/travel-review/create/[routeId]/page.tsx
+++ src/app/travel-review/create/[routeId]/page.tsx
@@ -3,21 +3,39 @@
 import BackNavigation from '@/components/common/backNavigation/BackNavigation';
 import Input from '@/components/common/inputs/Input';
 import { INFO_CONTAINER_STYLES, LABEL_STYLES, ROUTE_INFO_DUMMY_DATA } from './constants';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import StarRating from '@/components/travel-review/StarRatings';
 import RouteInfoContainer from '@/components/travel-review/RouteInfo';
-// import Button from '@/components/common/buttons/Button';
+import LayerPopup from '@/components/common/layerPopup/LayerPopup';
+import Button from '@/components/common/buttons/Button';
+import { useParams } from 'next/navigation';
 
 const Page = () => {
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const { routeId: travel_route_id } = useParams();
   const [showLayerPopup, setShowLayerPopup] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>('');
   const [rating, setRating] = useState<number | null>(0);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (titleRef.current) {
+      setTitle(titleRef.current.value);
+    }
+
     setShowLayerPopup(true);
   };
 
   // 제출 버튼 클릭 시 후기 등록 요청 필요
+  const postReview = () => {
+    const content = {
+      travel_route_id,
+      title,
+      rating,
+    };
+    console.log(content);
+  };
 
   return (
     <div className="flex flex-col items-center w-full">
@@ -29,6 +47,7 @@ const Page = () => {
         <div className={INFO_CONTAINER_STYLES}>
           <span className={LABEL_STYLES}>제목</span>
           <Input
+            ref={titleRef}
             id="title"
             type="text"
             variant="title"
@@ -46,10 +65,16 @@ const Page = () => {
         <RouteInfoContainer label="일정" content={ROUTE_INFO_DUMMY_DATA.title} />
         <RouteInfoContainer label="경로" content={ROUTE_INFO_DUMMY_DATA.route} />
 
-        {/* <Button type="submit" size="sm" color="blue" label="등록" className="self-end" onClick={handleSubmit}/> */}
+        <Button type="submit" size="sm" color="blue" label="등록" className="self-end" />
       </form>
 
-      {showLayerPopup && <>{/* <LayerPopup /> */}</>}
+      {showLayerPopup && (
+        <LayerPopup
+          label="후기를 등록하시겠습니까?"
+          setShowLayerPopup={setShowLayerPopup}
+          onConfirm={postReview}
+        />
+      )}
     </div>
   );
 };
