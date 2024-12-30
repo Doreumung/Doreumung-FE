9d7297f8b1a097628a3a124576f883b25c0c7f03
Kwonyeojiny <kangagi0009@naver.com>
2024-12-30 13:50:22 +0900

    feat: 단계별 진행도를 표시하는 프로그래스바 구현

--- src/components/travel-plan/form/MultiStepForm.tsx
+++ src/components/travel-plan/form/MultiStepForm.tsx
@@ -6,6 +6,7 @@ import SelectRegion from './SelectRegion';
 import SelectSchedule from './SelectSchedule';
 import TravelHeader from './TravelHeader';
 import BackNavigation from '@/components/common/backNavigation/BackNavigation';
+import ProgressIndicator from './ProgressIndicator';
 
 const MultiStepForm = () => {
   const [step, setStep] = useState(1);
@@ -20,38 +21,41 @@ const MultiStepForm = () => {
   const handleSubmit = () => {};
 
   return (
-    <div className="flex flex-col gap-2 min-h-screen px-8 pt-4 pb-8">
-      <header className="text-base">
-        <BackNavigation to="home" />
-      </header>
-      <main className="flex-grow">
-        <form>
-          {step === 1 && (
-            <div>
-              <TravelHeader step="지역" />
-              <SelectRegion />
-            </div>
+    <div>
+      <ProgressIndicator currentStep={step} totalSteps={2} />
+      <div className="flex flex-col gap-2 min-h-screen px-8 pt-4 pb-8">
+        <header className="text-base">
+          <BackNavigation to="home" />
+        </header>
+        <main className="flex-grow">
+          <form>
+            {step === 1 && (
+              <div>
+                <TravelHeader step="지역" />
+                <SelectRegion />
+              </div>
+            )}
+            {step === 2 && (
+              <div>
+                <TravelHeader step="테마" />
+                <SelectSchedule />
+              </div>
+            )}
+          </form>
+        </main>
+        <footer className="flex justify-between">
+          {step > 1 ? (
+            <Button size="md" color="yellow" label="이전" onClick={handelPrevStep} />
+          ) : (
+            <div></div>
           )}
-          {step === 2 && (
-            <div>
-              <TravelHeader step="테마" />
-              <SelectSchedule />
-            </div>
+          {step < 2 ? (
+            <Button size="md" color="yellow" label="다음" onClick={handelNextStep} />
+          ) : (
+            <Button size="md" color="blue" label="일정 생성" onClick={handleSubmit} />
           )}
-        </form>
-      </main>
-      <footer className="flex justify-between">
-        {step > 1 ? (
-          <Button size="md" color="yellow" label="이전" onClick={handelPrevStep} />
-        ) : (
-          <div></div>
-        )}
-        {step < 2 ? (
-          <Button size="md" color="yellow" label="다음" onClick={handelNextStep} />
-        ) : (
-          <Button size="md" color="blue" label="일정 생성" onClick={handleSubmit} />
-        )}
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 };
