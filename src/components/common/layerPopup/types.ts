be85b23ae58976e1b17bc04879f6e3ab6ba1491e
xuuwon <dumpling1011@gmail.com>
2024-12-31 01:47:22 +0900

    refactor: 불필요한 props 제거 및 스타일 분리, 부모로 데이터 전달을 위한 props 추가

--- src/components/common/layerPopup/types.ts
+++ src/components/common/layerPopup/types.ts
@@ -5,6 +5,6 @@ import { HTMLAttributes } from 'react';
 export type LayerPopupProps = HTMLAttributes<HTMLDivElement> &
   VariantProps<typeof layerPopupStyles> & {
     label?: React.ReactNode;
-    children?: React.ReactElement;
-    onClose?: () => void;
+    type?: 'confirm' | 'input';
+    onConfirm?: (title: string) => void; // title 데이터 부모 컴포넌트로 올리기
   };
