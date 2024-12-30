cb5a1c12b8643cac24a472a9d71b41f326b03144
Seyoon Jeong <seyoonagain@gmail.com>
2024-12-31 02:10:45 +0900

    fix: Button 스타일 색상명과 배경 색상명 통일 누락된 부분 재수정

--- src/components/common/buttons/buttonStyles.ts
+++ src/components/common/buttons/buttonStyles.ts
@@ -16,8 +16,8 @@ export const buttonStyles = cva('border border-darkerGray rounded-2xl', {
       blue: 'bg-blue text-background',
       fadedGreen: 'bg-fadedGreen text-foreground',
       fadedYellow: 'bg-fadedYellow text-foreground',
-      lightGray: 'bg-lighterGray text-foreground',
-      darkGray: 'bg-darkerGray text-background',
+      lighterGray: 'bg-lighterGray text-foreground',
+      darkerGray: 'bg-darkerGray text-background',
     },
     shadow: {
       dropShadow:
