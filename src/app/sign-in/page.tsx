bd271f8035c77b4d430070db5133cac727acf4c4
xuuwon <dumpling1011@gmail.com>
2024-12-31 03:28:44 +0900

    feat: 로그인 유효성 검사, 라우팅 및 UI 수정

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -7,9 +7,19 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { signInSchema, SignInSchema } from './signInSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const sampleUserData = {
+    // 샘플 데이터
+    email: 'dumpling0227@naver.com',
+    password: 'abc12345!',
+  };
 
   const {
     register, // 연결하여 유효성 검사 진행
@@ -22,76 +32,107 @@ const Page = () => {
   });
 
   const onSubmit = (data: SignInSchema) => {
-    // 제출했을 때 data 반환
-    const userData = { ...data, isChecked };
-    console.log(userData);
+    if (email === sampleUserData.email && password === sampleUserData.password) {
+      setErrorMessage('');
 
-    // 확인용
-    console.log('Form Data:', data);
-    console.log(isChecked);
+      // 로그인 성공했을 때 data 반환
+      const userData = { ...data, isChecked };
+      console.log(userData);
+    } else {
+      setErrorMessage('이메일 혹은 비밀번호를 확인해주세요.');
+    }
+  };
+
+  // 회원가입 버튼
+  const router = useRouter();
+
+  const goToSignup = () => {
+    router.push('/sign-up'); // '/sign-up' 페이지로 이동
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col items-center justify-center h-screen"
-    >
-      <p className="pb-8 text-3xl text-darkerGray">로그인</p>
-      <div className="flex flex-col gap-4">
-        <Input id="email" label="이메일" type="email" variant="signin" {...register('email')} />
-        {errors.email && <p className="px-3 pb-1 text-xs text-red-600">{errors.email.message}</p>}
-        <Input
-          id="password"
-          label="비밀번호"
-          type="password"
-          variant="signin"
-          {...register('password')}
-        />
-        {errors.password && (
-          <p className="px-3 pb-1 text-xs text-red-600">{errors.password.message}</p>
-        )}
-        <div className="flex gap-1.5 pb-10 px-2 text-darkGray">
-          <div
-            className={`w-5 h-5 rounded-md ${
-              isChecked ? 'bg-green' : 'border border-green bg-background'
-            } flex items-center justify-center transition-transform duration-200 transform active:scale-125`}
-            onClick={() => {
-              setIsChecked(!isChecked);
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center">
+        <p className="pb-8 text-3xl text-darkerGray">로그인</p>
+        <div className="flex flex-col gap-3 w-96">
+          <Input
+            id="email"
+            label="이메일"
+            type="email"
+            variant="signin"
+            {...register('email')}
+            value={email}
+            onChange={event => {
+              setEmail(event.target.value);
             }}
-          >
-            {isChecked && (
-              <svg
-                className="w-5 h-5 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-            )}
+          />
+          <Input
+            id="password"
+            label="비밀번호"
+            type="password"
+            variant="signin"
+            {...register('password')}
+            value={password}
+            onChange={event => {
+              setPassword(event.target.value);
+            }}
+          />
+          <div className="flex gap-1.5 pb-10 px-2 text-darkGray">
+            <div
+              className={`w-5 h-5 rounded-md ${
+                isChecked ? 'bg-green' : 'border border-green bg-background'
+              } flex items-center justify-center transition-transform duration-200 transform active:scale-125`}
+              onClick={() => {
+                setIsChecked(!isChecked);
+              }}
+            >
+              {isChecked && (
+                <svg
+                  className="w-5 h-5 text-white"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+              )}
+            </div>
+            <p>자동 로그인</p>
           </div>
-          <p>자동 로그인</p>
         </div>
-      </div>
-      <div className="flex flex-col gap-10">
-        <Button label="로그인" onClick={() => {}} className="w-96 text-sm" />
-        <div className="flex justify-center gap-2 pb-10 text-lightGray">
-          <p>아직 회원이 아니신가요?</p>
-          <button className="text-darkerGray underline underline-offset-4">회원가입</button>
+        {/* 이메일 에러 우선 표시 */}
+        {errors.email ? (
+          <p className="px-3 pb-4 text-xs text-red">{errors.email.message}</p>
+        ) : errors.password ? (
+          <p className="px-3 pb-4 text-xs text-red">{errors.password.message}</p>
+        ) : errorMessage ? (
+          <p className="px-3 pb-4 text-xs text-red">{errorMessage}</p>
+        ) : null}
+        <div className="flex flex-col items-center gap-10">
+          <Button label="로그인" className="w-96 text-sm" />
+          <div className="flex justify-center gap-2 pb-10 text-lightGray">
+            <p>아직 회원이 아니신가요?</p>
+            <span
+              className="text-darkerGray underline underline-offset-4"
+              onClick={() => goToSignup()}
+            >
+              회원가입
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-4">
+      </form>
+      <div className="flex flex-col items-center gap-4">
         <SocialLoginButton provider="kakao" onClick={() => {}} />
         <SocialLoginButton provider="naver" onClick={() => {}} />
         <SocialLoginButton provider="google" onClick={() => {}} />
       </div>
-    </form>
+    </div>
   );
 };
 
--- src/app/sign-in/signInSchema.ts
+++ src/app/sign-in/signInSchema.ts
@@ -1,18 +1,11 @@
 import { z } from 'zod';
 
+// 공통 유효성 검사 정의
+const requiredField = () => z.string().nonempty(`이메일 혹은 비밀번호를 입력해주세요.`).trim();
+
 export const signInSchema = z.object({
-  email: z
-    .string()
-    .nonempty('이메일을 입력해주세요.') // 필수 입력
-    .email('유효한 이메일 주소를 입력해주세요.'), // 형식 검사
-  password: z
-    .string()
-    .trim()
-    .max(12, '비밀번호는 6자 이상, 12자 이하로 입력 가능합니다.')
-    .min(6, '비밀번호는 6자 이상, 12자 이하로 입력 가능합니다.')
-    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,12}$/, {
-      message: `비밀번호는 영문, 숫자, 특수문자를 포함하여야 합니다.`,
-    }),
+  email: requiredField(), // 이메일 필수 입력
+  password: requiredField(), // 비밀번호 필수 입력
 });
 
-export type SignInSchema = z.infer<typeof signInSchema>; //
+export type SignInSchema = z.infer<typeof signInSchema>;
