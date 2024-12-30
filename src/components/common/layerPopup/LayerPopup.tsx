8d047792cd7c47c9c53e3434ced20c77f665bf1c
Seyoon Jeong <seyoonagain@gmail.com>
2024-12-31 02:47:44 +0900

    fix: Button 컴포넌트 수정에 따른 LayerPopup 업데이트

--- src/components/common/layerPopup/LayerPopup.tsx
+++ src/components/common/layerPopup/LayerPopup.tsx
@@ -1,30 +1,40 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { clsx } from 'clsx';
 import { LayerPopupProps } from './types';
 import { layerPopupStyles } from './layerPopupStyles';
 import Button from '../buttons/Button';
 import Image from 'next/image';
 import Input from '../inputs/Input';
+import Dolmung from '@public/images/dolmung.svg';
 
-const LayerPopup: React.FC<LayerPopupProps> = ({ label, type, onConfirm, ...props }) => {
+const LayerPopup: React.FC<LayerPopupProps> = ({
+  label,
+  type = 'confirm',
+  onConfirm,
+  setShowLayerPopup,
+  ...props
+}) => {
   const isConfirmPopup = type === 'confirm'; // 입력창 없는 팝업
-  const [isVisible, setIsVisible] = useState<boolean>(true); // 취소 버튼 누르면 닫기
-
   const [titleData, setTitleData] = useState<string>(''); // 제목 지정
 
   const handleConfirm = () => {
-    if (onConfirm) {
+    if (isConfirmPopup) {
+      onConfirm();
+    } else {
       onConfirm(titleData); // 부모 컴포넌트에 데이터 전달
     }
-    setIsVisible(false); // 팝업 닫기
+    setShowLayerPopup(false); // 팝업 닫기
+  };
+
+  const handleCancel = () => {
+    setShowLayerPopup(false);
   };
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
-        setIsVisible(false);
+        setShowLayerPopup(false);
       }
     };
 
@@ -33,33 +43,21 @@ const LayerPopup: React.FC<LayerPopupProps> = ({ label, type, onConfirm, ...prop
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [isVisible]);
+  }, [setShowLayerPopup]);
 
   return (
-    <div
-      className={clsx(
-        'flex justify-center items-center fixed inset-0 bg-overlay transition-opacity z-50',
-        isVisible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none',
-      )}
-    >
+    <div className="flex justify-center items-center fixed inset-0 bg-overlay transition-opacity z-50 opacity-100 pointer-events-auto">
       {isConfirmPopup ? (
         <div className={layerPopupStyles()} {...props}>
           <div className="relative p-6 h-50 rounded-2xl">
             <div className="flex flex-col gap-20">
               <div className="flex justify-start items-center gap-5 text-base">
-                <Image src="/images/dolmung.svg" alt="dolmung image" width={60} height={60} />
-                {label && label}
+                <Image src={Dolmung} alt="dolmung image" width={60} height={60} />
+                {label}
               </div>
               <div className="flex justify-end gap-3">
-                <Button
-                  label="취소"
-                  size={'xs'}
-                  color="lightGray"
-                  onClick={() => {
-                    setIsVisible(false);
-                  }}
-                />
-                <Button label="확인" size={'xs'} onClick={() => {}} />
+                <Button label="취소" size="xs" color="lighterGray" onClick={handleCancel} />
+                <Button label="확인" size="xs" onClick={handleConfirm} />
               </div>
             </div>
           </div>
@@ -69,8 +67,8 @@ const LayerPopup: React.FC<LayerPopupProps> = ({ label, type, onConfirm, ...prop
           <div className="relative w-full p-6 h-62 rounded-2xl">
             <div className="flex flex-col items-center gap-5">
               <div className="flex items-center self-start gap-3 pl-6 text-base">
-                <Image src="/images/dolmung.svg" alt="dolmung image" width={50} height={50} />
-                {label && label}
+                <Image src={Dolmung} alt="dolmung image" width={50} height={50} />
+                {label}
               </div>
               <Input
                 id="title"
@@ -82,22 +80,8 @@ const LayerPopup: React.FC<LayerPopupProps> = ({ label, type, onConfirm, ...prop
                 onChange={event => setTitleData(event.target.value)}
               />
               <div className="flex self-end gap-3 pt-5">
-                <Button
-                  label="취소"
-                  size={'xs'}
-                  color="lightGray"
-                  onClick={() => {
-                    setIsVisible(false);
-                  }}
-                />
-                <Button
-                  label="확인"
-                  size={'xs'}
-                  onClick={() => {
-                    handleConfirm();
-                    console.log(titleData); // 확인용
-                  }}
-                />
+                <Button label="취소" size="xs" color="lighterGray" onClick={handleCancel} />
+                <Button label="확인" size="xs" onClick={handleConfirm} />
               </div>
             </div>
           </div>
