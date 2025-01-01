877dfb112f1857b505522fb2d262681a626b55fc
Kwonyeojiny <kangagi0009@naver.com>
2025-01-02 03:44:29 +0900

    design: 모바일 버전 토글 버튼 스타일 수정

--- src/components/common/toggle/toggleStyles.ts
+++ src/components/common/toggle/toggleStyles.ts
@@ -3,12 +3,12 @@ import { cva } from 'class-variance-authority';
 export const toggleStyles = cva('rounded-2xl border border-darkerGray', {
   variants: {
     size: {
-      md: 'min-w-24 h-10 px-5 text-xl',
+      md: 'min-w-20 h-10 px-3 text-base md:min-w-24 md:px-5 md:text-xl',
       sm: 'w-20 h-9 text-base',
     },
     checked: {
       true: 'bg-fadedSkyblue text-darkerGray',
-      false: 'bg-lighterGray text-lightGray',
+      false: 'bg-lighterGray text-lightGray hover:bg-fadedSkyblue',
     },
     disabled: {
       true: '!bg-darkerGray !text-lighterGray',
