7dec2abfcd4cab7f9976e23bb85a58e7228531bc
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-02 01:34:19 +0900

    design: 여행 후기 페이지 하단 padding 값 설정

--- src/app/travel-review/layout.tsx
+++ src/app/travel-review/layout.tsx
@@ -1,6 +1,6 @@
 const TravelReviewLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-full max-w-screen-lg px-4 pt-4 md:px-12 md:pt-8">{children}</section>
+    <section className="w-full max-w-screen-lg px-4 pt-4 pb-4 md:px-12 md:pt-8">{children}</section>
   );
 };
 
