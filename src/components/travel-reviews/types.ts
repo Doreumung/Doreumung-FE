ad5abdda007a3e252d587a7478c8bc4202b4a424
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-02 14:26:44 +0900

    feat: RouteInfo 컴포넌트를 RouteInfoContainer로 이름 변경 및 cva 적용

--- src/components/travel-reviews/types.ts
+++ src/components/travel-reviews/types.ts
@@ -1,14 +1,16 @@
 import { Dispatch, SetStateAction } from 'react';
 import { ChainedCommands, Editor } from '@tiptap/react';
 import { LucideIcon } from 'lucide-react';
+import { VariantProps } from 'class-variance-authority';
+import { routeInfoContainerStyles } from './RouteInfoStyles';
 
 export type StarRatingProps = {
   rating: number | null;
   setRating: Dispatch<SetStateAction<number | null>>;
 };
 
-export type RouteInfoProps = {
-  label: '일정' | '경로';
+export type RouteInfoProps = VariantProps<typeof routeInfoContainerStyles> & {
+  label: '일정' | '경로' | '여행 지역' | '여행 경로';
   content: string;
 };
 
