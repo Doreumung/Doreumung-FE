88ed52de8cbf9bed34e9ab8438c15191a62c6afd
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-05 22:45:53 +0900

    feat: BackNavigation을 통해 목록이 아닌 수정 중인 후기의 상세 페이지로 이동

--- src/app/travel-reviews/edit/[reviewId]/page.tsx
+++ src/app/travel-reviews/edit/[reviewId]/page.tsx
@@ -2,11 +2,13 @@
 
 import BackNavigation from '@/components/common/backNavigation/BackNavigation';
 import ReviewForm from '@/components/travel-reviews/reviewForm/ReviewForm';
+import { useParams } from 'next/navigation';
 
 const Page = () => {
+  const { reviewId } = useParams();
   return (
     <div className="flex flex-col items-center w-full">
-      <BackNavigation to="reviewList" />
+      <BackNavigation to="review" reviewId={reviewId as string} />
       <h3 className="block py-12 text-darkerGray text-3xl">후기 수정</h3>
       <ReviewForm mode="edit" />
     </div>
