0f601bf3a3a82fd52a6173d76d8608148e3f614e
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-06 05:12:31 +0900

    feat: store에 commentApi 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import travelPlanReducer from './travelPlanSlice';
 import reviewApi from '@/api/reviewApi';
+import commentApi from '@/api/commentApi';
 import { userApi } from '@/api/userApi';
 
 export const store = configureStore({
   reducer: {
     travelPlan: travelPlanReducer,
     [reviewApi.reducerPath]: reviewApi.reducer,
+    [commentApi.reducerPath]: commentApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(reviewApi.middleware).concat(userApi.middleware),
+    getDefaultMiddleware() //
+      .concat(reviewApi.middleware, commentApi.middleware, userApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
