58d1ef3f6facd88a3694fa31c59e125105efc804
xuuwon <dumpling1011@gmail.com>
2025-01-07 01:01:12 +0900

    feat: 로그인 및 유저 정보 받아오기 API 요청 코드 작성

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,5 +1,6 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import baseQuery from './baseQuery';
+import { UserState } from '@/store/userSlice';
 
 export const userApi = createApi({
   reducerPath: 'userApi', // 리듀서 경로 이름
@@ -15,7 +16,23 @@ export const userApi = createApi({
         },
       }),
     }),
+    login: builder.mutation({
+      query: newUser => ({
+        url: '/user/login', // 로그인 엔드포인트
+        method: 'POST',
+        body: newUser, // 요청 데이터
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+    getUserInfo: builder.query<UserState['user'], void>({
+      query: () => ({
+        url: '/user/me', // 회원정보 받아오기 엔드포인트
+        method: 'GET',
+      }),
+    }),
   }),
 });
 
-export const { useSignUpMutation } = userApi;
+export const { useSignUpMutation, useLoginMutation, useGetUserInfoQuery } = userApi;
