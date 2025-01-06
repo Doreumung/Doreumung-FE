d028da2658c0e57321e6b47d8beec1c1130e3920
xuuwon <dumpling1011@gmail.com>
2025-01-07 01:05:31 +0900

    feat: Providers 컴포넌트 호출 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import Header from '@/components/common/header/Header';
-import ClientProvider from './ClientProvider';
+import Providers from '@/components/providers/Providers';
 
 export const metadata: Metadata = {
   title: '도르멍',
@@ -16,10 +16,10 @@ const RootLayout = ({
   return (
     <html lang="ko">
       <body>
-        <ClientProvider>
+        <Providers>
           <Header />
           <main className="flex justify-center w-full mt-16 md:mt-20">{children}</main>
-        </ClientProvider>
+        </Providers>
       </body>
     </html>
   );
