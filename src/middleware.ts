180cfd3e4f339ea6af1874bc2111b9b1ee39cf80
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-07 18:56:17 +0900

    refactor: 미들웨어 코드 리팩토링

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,29 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export const middleware = (request: NextRequest) => {
+const membersOnlyRoutes = ['/my-travel'];
+const guestsOnlyRoutes = ['/sign-up', '/sign-in'];
+
+export function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken');
   const { pathname } = request.nextUrl;
   const url = request.nextUrl.clone();
 
-  if (!token && membersOnlyRoutes === pathname) {
+  if (!token && membersOnlyRoutes.includes(pathname)) {
     url.pathname = '/redirect';
     url.searchParams.set('mode', 'NOT_SIGNED_IN');
     return NextResponse.redirect(url);
   }
 
-  if (token && guestsOnlyRoutes.some(route => pathname.startsWith(route))) {
+  if (token && guestsOnlyRoutes.includes(pathname)) {
     url.pathname = '/redirect';
     url.searchParams.set('mode', 'SIGNED_IN');
     return NextResponse.redirect(url);
   }
-};
 
-const membersOnlyRoutes = '/my-travel';
-const guestsOnlyRoutes = ['/sign-up', '/sign-in'];
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/my-travel', '/sign-up', '/sign-in'],
+  matcher: [...membersOnlyRoutes, ...guestsOnlyRoutes],
 };
