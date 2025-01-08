cf5a3155f6570dc7d52528207dd865a69784943d
xuuwon <dumpling1011@gmail.com>
2025-01-08 19:06:47 +0900

    feat: Redux 상태 업데이트 시 기본값 설정 및 로그인 방식 저장

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -10,12 +10,13 @@ export type UserState = {
     created_at: string;
     updated_at: string;
   } | null;
+  loginType: 'kakao' | 'google' | 'email';
 };
 
 const initialState: UserState = {
   // 다시 수정
   user: {
-    id: '',
+    id: 'default_id',
     email: '',
     nickname: '',
     gender: 'none',
@@ -23,15 +24,27 @@ const initialState: UserState = {
     created_at: '',
     updated_at: '',
   },
+  loginType: 'email',
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser(state, action: PayloadAction<{ user: UserState['user'] }>) {
-      console.log('setUser 액션 호출:', action.payload); // 확인용
-      state.user = action.payload.user;
+    setUser(
+      state,
+      action: PayloadAction<{ user: UserState['user']; loginType: 'kakao' | 'google' | 'email' }>,
+    ) {
+      const user = action.payload.user;
+
+      state.user = user
+        ? {
+            ...user,
+            gender: user.gender || 'none',
+            birthday: user.birthday || '1925-01-01',
+          }
+        : null;
+      state.loginType = action.payload.loginType; // 로그인 방식 저장
     },
     clearUser(state) {
       state.user = null;
