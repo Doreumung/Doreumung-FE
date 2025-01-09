7ec84893b42d33ec3909993feefa7bbdb203ae75
xuuwon <dumpling1011@gmail.com>
2025-01-09 10:48:03 +0900

    style: 레이어 팝업 최대 너비 추가

--- src/components/common/layerPopup/layerPopupStyles.ts
+++ src/components/common/layerPopup/layerPopupStyles.ts
@@ -1,5 +1,5 @@
 import { cva } from 'class-variance-authority';
 
 export const layerPopupStyles = cva(
-  'w-[calc(100%-32px)] md:w-[535px] border border-darkerGray text-darkerGray rounded-2xl bg-background',
+  'w-[calc(100%-32px)] md:w-[535px] max-w-[535px] border border-darkerGray text-darkerGray rounded-2xl bg-background',
 );
