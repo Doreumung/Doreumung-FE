21e10283eedd719f0041746eaa3122a8c8138de9
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-10 03:13:19 +0900

    feat: 여행 후기 작성에 필요한 여행 경로 정보 GET 요청

--- src/app/travel-reviews/create/[routeId]/page.tsx
+++ src/app/travel-reviews/create/[routeId]/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 
+import { useGetTravelRouteInfoQuery } from '@/api/reviewApi';
 import BackNavigation from '@/components/common/backNavigation/BackNavigation';
 import ReviewForm from '@/components/travel-reviews/reviewForm/ReviewForm';
+import { useParams } from 'next/navigation';
 
 const Page = () => {
+  const { routeId } = useParams();
+  const { data: travelRouteInfo } = useGetTravelRouteInfoQuery(
+    { travel_route_id: Number(routeId) },
+    { skip: !routeId },
+  );
+
   return (
     <div className="flex flex-col items-center w-full">
       <BackNavigation to="reviewList" />
       <h3 className="block py-12 text-darkerGray text-3xl">후기 작성</h3>
-      <ReviewForm mode="create" />
+      {travelRouteInfo && <ReviewForm mode="create" travelRouteInfo={travelRouteInfo} />}
     </div>
   );
 };
