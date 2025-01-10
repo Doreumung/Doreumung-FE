f447abeca04b72e48101bcafb3ecabcf4a7d620d
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-10 14:16:05 +0900

    feat: toast 전역 상태로 관리함에 따라Toast 컴포넌트 수정

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,59 +1,34 @@
+'use client';
+
 import clsx from 'clsx';
 import { toastStyles } from './toastStyles';
-import React, { useEffect, useState } from 'react';
-import { SingleToast, ToastArgs, Toasts, ToastTriggerArgs } from './types';
+import { ToastArgs } from './types';
 import { X } from 'lucide-react';
-
-let toastTrigger: ({ message, type }: ToastTriggerArgs) => void;
+import { RootState, store } from '@/store/store';
+import { addToast, hideToast, removeToast, showToast } from '@/store/toastSlice';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { v4 as uuid } from 'uuid';
 
 export const toast = ({ message, type = 'success' }: ToastArgs) => {
-  if (toastTrigger) toastTrigger({ message, type });
+  const id = uuid();
+  store.dispatch(addToast({ id, message, type }));
+  setTimeout(() => store.dispatch(showToast(id)), 300);
+  setTimeout(() => store.dispatch(hideToast(id)), 5000);
+  setTimeout(() => store.dispatch(removeToast(id)), 5300);
 };
 
 const Toast = () => {
-  const [toasts, setToasts] = useState<Toasts>([]);
-  const [toastId, setToastId] = useState<number>(0);
+  const dispatch = useAppDispatch();
+  const toasts = useAppSelector((state: RootState) => state.toast.toasts);
 
-  const handleClickClose = (id: number) => {
-    setToasts(prev => prev.filter(toast => toast.id !== id));
+  const handleClickClose = (id: string) => {
+    dispatch(removeToast(id));
   };
 
-  useEffect(() => {
-    toastTrigger = ({ message, type }) => {
-      const newToast: SingleToast = {
-        id: toastId,
-        type,
-        message,
-        visible: false,
-      };
-
-      setToasts(prev => [...prev, newToast]);
-      setToastId(prev => prev + 1);
-
-      setTimeout(
-        () =>
-          setToasts(prev =>
-            prev.map(toast => (toast.id === newToast.id ? { ...toast, visible: true } : toast)),
-          ),
-        300,
-      );
-
-      setTimeout(
-        () =>
-          setToasts(prev =>
-            prev.map(toast => (toast.id === newToast.id ? { ...toast, visible: false } : toast)),
-          ),
-        5000,
-      );
-
-      setTimeout(() => setToasts(prev => prev.filter(toast => toast.id !== newToast.id)), 5300);
-    };
-  }, [toastId]);
-
-  if (toast.length === 0) return null;
+  if (toasts.length === 0) return null;
 
   return (
-    <div className="flex flex-col-reverse gap-4 fixed right-0 sm:right-4 bottom-4 w-full max-w-96 px-4">
+    <div className="flex flex-col-reverse gap-4 fixed right-0 sm:right-4 bottom-4 w-full max-w-md px-4">
       {toasts.map(({ id, type, message, visible }) => (
         <div
           key={id}
@@ -72,7 +47,11 @@ const Toast = () => {
                 type === 'error' && 'bg-[url(/images/sadDolmung.svg)]',
               )}
             />
-            <p>{message}</p>
+            <div className="flex flex-col gap-1">
+              {message.map(row => (
+                <p key={`${id}-${row}`}>{row}</p>
+              ))}
+            </div>
           </div>
           <X
             size={18}
