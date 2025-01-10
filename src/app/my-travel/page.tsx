0c8c13f8f58869d600be3c48789820cec7e48624
Kwonyeojiny <kangagi0009@naver.com>
2025-01-10 15:33:40 +0900

    feat: 사용자 저장 여행 경로 목록 페이지네이션 추가

--- src/app/my-travel/page.tsx
+++ src/app/my-travel/page.tsx
@@ -1,14 +1,26 @@
 'use client';
 
+import { useState } from 'react';
 import TravelCard from '@/components/my-travel/TravelCard';
 import { useGetTraveRoutesQuery } from '@/api/travelRouteApi';
 import LoadingSpinner from '@/components/common/loadingSpinner/LoadingSpinner';
+import Pagination from '@/components/common/pagination/Pagination';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 
 const Page = () => {
-  const { data: travelRoute, isLoading, error } = useGetTraveRoutesQuery({ page: 1, size: 5 });
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+  const {
+    data: travelRoute,
+    isLoading,
+    error,
+  } = useGetTraveRoutesQuery({
+    page: currentPage,
+    size: itemsPerPage,
+  });
   const userData = useSelector((state: RootState) => state.user.user);
+
   if (isLoading)
     return (
       <div>
@@ -21,18 +33,23 @@ const Page = () => {
   return (
     <div className="flex flex-col items-center pt-4 overflow-x-hidden pb-10">
       <p className="py-16 text-3xl">{userData?.nickname}님의 저장 경로</p>{' '}
-      {/* 사용자 이름 받아와서 적용 */}
       <div className="flex flex-col w-full max-w-[768px] px-4 mx-auto gap-8 md:pb-10">
         {travelRoute.travel_list.map(route => (
           <div key={route.travel_route_id} className="flex-shrink-0">
             <TravelCard
               title={route.title}
               region={route.config.regions}
-              placeArray={route.travel_route} // travel_route 사용
+              placeArray={route.travel_route}
             />
           </div>
         ))}
       </div>
+      <Pagination
+        totalResults={travelRoute.total_travel_routes}
+        currentPage={currentPage}
+        setPage={setCurrentPage}
+        perPage={itemsPerPage}
+      />
     </div>
   );
 };
