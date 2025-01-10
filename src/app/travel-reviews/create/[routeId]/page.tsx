f62ed503f7d8d285163dfa785669d679c8fb2c9e
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-11 03:13:26 +0900

    chore: API 에러 메세지를 ApiErrorMessage 컴포넌트로 대체

--- src/app/travel-reviews/create/[routeId]/page.tsx
+++ src/app/travel-reviews/create/[routeId]/page.tsx
@@ -2,21 +2,32 @@
 
 import { useGetTravelRouteInfoQuery } from '@/api/reviewApi';
 import BackNavigation from '@/components/common/backNavigation/BackNavigation';
+import ApiErrorMessage from '@/components/common/errorMessage/ApiErrorMessage';
+import LoadingSpinner from '@/components/common/loadingSpinner/LoadingSpinner';
 import ReviewForm from '@/components/travel-reviews/reviewForm/ReviewForm';
 import { useParams } from 'next/navigation';
 
 const Page = () => {
   const { routeId } = useParams();
-  const { data: travelRouteInfo } = useGetTravelRouteInfoQuery(
-    { travel_route_id: Number(routeId) },
-    { skip: !routeId },
-  );
+  const {
+    data: travelRouteInfo,
+    isLoading,
+    error,
+  } = useGetTravelRouteInfoQuery({ travel_route_id: Number(routeId) }, { skip: !routeId });
+  if (isLoading) <LoadingSpinner />;
 
   return (
     <div className="flex flex-col items-center w-full">
       <BackNavigation to="reviewList" />
-      <h3 className="block py-12 text-darkerGray text-3xl">후기 작성</h3>
-      {travelRouteInfo && <ReviewForm mode="create" travelRouteInfo={travelRouteInfo} />}
+
+      {error && <ApiErrorMessage />}
+
+      {!error && (
+        <>
+          <h3 className="block py-12 text-darkerGray text-3xl">후기 작성</h3>
+          {travelRouteInfo && <ReviewForm mode="create" travelRouteInfo={travelRouteInfo} />}
+        </>
+      )}
     </div>
   );
 };
