3b7837b813b1216850fd03d250d28fb162a9e173
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-12 04:30:32 +0900

    chore: toast의 매개변수 상수로 대체

--- src/components/travel-reviews/reviewCard/ReviewCard.tsx
+++ src/components/travel-reviews/reviewCard/ReviewCard.tsx
@@ -7,16 +7,16 @@ const ReviewCard = ({ review }: { review: SingleReviewType }) => {
   return (
     <div className="w-full max-w-80 h-[400px] border border-darkerGray rounded-2xl bg-white text-darkerGray overflow-hidden ">
       <Link href={`travel-reviews/detail/${review.review_id}`}>
-        <section className="h-2/3 border-b border-darkerGray bg-fadedSkyblue">
+        <section className="h-3/5 border-b border-darkerGray bg-fadedSkyblue">
           {/* 후기 대표 사진 삽입 필요*/}
         </section>
       </Link>
 
-      <section className="flex flex-col gap-1 w-full h-1/3 px-4 py-3">
+      <section className="flex flex-col gap-1 w-full h-2/5 px-4 py-3">
         <ReviewStats stats={review.rating} color="yellow" icon={Star} />
 
         <Link href={`travel-reviews/detail/${review.review_id}`} className="flex-grow">
-          <span className="text-lg text-foreground">{review.title}</span>
+          <span className="text-lg text-foreground line-clamp-2">{review.title}</span>
         </Link>
 
         <div className="flex justify-between items-center">
