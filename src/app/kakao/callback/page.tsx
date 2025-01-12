d07353010be6b4cd20b62f9367ceb730afab8351
xuuwon <dumpling1011@gmail.com>
2025-01-13 03:07:08 +0900

    feat: 토큰 유효기간 수정 및 로컬 스토리지에 유효기간 저장 로직 추가 및 새로고침 추가

--- src/app/kakao/callback/page.tsx
+++ src/app/kakao/callback/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useGetUserInfoMutation, useSendKakaoCodeQuery } from '@/api/userApi';
+import { setCookieWithExpiry } from '@/app/sign-in/setCookieWithExpiry';
 import LoadingSpinner from '@/components/common/loadingSpinner/LoadingSpinner';
 import { setUser } from '@/store/userSlice';
 import { useRouter } from 'next/navigation';
-import { setCookie } from 'nookies';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -26,14 +26,9 @@ const KakaoCallback = () => {
   useEffect(() => {
     if (data) {
       console.log('토큰 정보:', data);
-      setCookie(null, 'access_token', data.access_token, {
-        maxAge: 30 * 24 * 60 * 60, // 쿠키 유효기간
-        path: '/', // 쿠키 경로
-      });
-      setCookie(null, 'refresh_token', data.refresh_token, {
-        maxAge: 30 * 24 * 60 * 60,
-        path: '/',
-      });
+
+      setCookieWithExpiry('access_token', data?.access_token, 24 * 60 * 60);
+      setCookieWithExpiry('refresh_token', data?.refresh_token, 7 * 24 * 60 * 60);
 
       // 로그인 성공 후 유저 데이터 받아오기
       getUserInfo({})
@@ -41,7 +36,12 @@ const KakaoCallback = () => {
         .then(userData => {
           console.log('유저 정보:', userData);
           dispatch(setUser({ user: userData, loginType: 'kakao' })); // Redux에 유저 정보 저장
+
+          localStorage.setItem('toast_shown', 'false');
+
+          // 홈으로 이동 후 새로고침
           router.push('/'); // 홈으로 이동
+          window.location.href = '/'; // 새로고침
         })
         .catch(error => {
           console.error('유저 정보 요청 실패:', error);
