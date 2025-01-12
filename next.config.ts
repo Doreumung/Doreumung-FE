aa0672a019e641402319a2272ac3f8f369f89f89
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-13 05:41:38 +0900

    chore: 이미지 경로 추가

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'doreumung-06.s3.ap-northeast-2.amazonaws.com',
         pathname: '*/*',
       },
+      {
+        protocol: 'https',
+        hostname: 'doreumung-06.s3.amazonaws.com',
+        pathname: '*/*',
+      },
     ],
   },
 };
