41b9696b30d7d269cab3752aa1cac3b107a94961
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-13 04:11:57 +0900

    feat: 웹소켓을 이용하여 댓글 실시간 업데이트 구현 및 댓글 UI 업데이트

--- src/components/travel-reviews/comment/CommentForm.tsx
+++ src/components/travel-reviews/comment/CommentForm.tsx
@@ -4,21 +4,18 @@ import { CommentFormType } from '@/app/travel-reviews/types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import ErrorMessage from '@/components/common/errorMessage/ErrorMessage';
-import { useEditCommentMutation, usePostCommentMutation } from '@/api/commentApi';
-import { toast } from '@/components/common/toast/Toast';
 import { useParams } from 'next/navigation';
 import { CommentFormProps } from '../types';
-import {
-  EDIT_COMMENT_ERROR_MESSAGE,
-  EDIT_COMMENT_SUCCESS_MESSAGE,
-  POST_COMMENT_ERROR_MESSAGE,
-  POST_COMMENT_SUCCESS_MESSAGE,
-} from '../constants';
+import { useAppSelector } from '@/store/hooks';
+import { RootState } from '@/store/store';
+import { useWebSocketContext } from '@/contexts/useWebSocketContext';
 
 const CommentForm = ({ content = '', setShowForm, comment_id }: CommentFormProps) => {
   const { reviewId: review_id } = useParams();
-  const [postComment] = usePostCommentMutation();
-  const [editComment] = useEditCommentMutation();
+  const user = useAppSelector((state: RootState) => state.user.user);
+  const { sendJsonMessage, isSocketOpen } = useWebSocketContext();
+
+  const user_id = user ? user.id : '';
 
   const {
     register,
@@ -32,33 +29,35 @@ const CommentForm = ({ content = '', setShowForm, comment_id }: CommentFormProps
 
   const onSubmit: SubmitHandler<CommentFormType> = data => {
     const comment = data.content;
-    if (!comment_id) {
-      postComment({ review_id: Number(review_id), content: comment })
-        .unwrap()
-        .then(() => {
-          reset({ content: '' });
-          toast(POST_COMMENT_SUCCESS_MESSAGE);
-        })
-        .catch(() => toast(POST_COMMENT_ERROR_MESSAGE));
-    } else if (setShowForm && comment_id) {
-      editComment({ comment_id, content: comment })
-        .unwrap()
-        .then(res => {
-          setShowForm(false);
-          reset({ content: res.content });
-          toast(EDIT_COMMENT_SUCCESS_MESSAGE);
-        })
-        .catch(() => toast(EDIT_COMMENT_ERROR_MESSAGE));
+    if (isSocketOpen) {
+      if (!comment_id) {
+        sendJsonMessage(
+          JSON.stringify({ type: 'comment', method: 'POST', review_id, content: comment, user_id }),
+        );
+        reset({ content: '' });
+      } else if (setShowForm && comment_id) {
+        sendJsonMessage(
+          JSON.stringify({
+            type: 'comment',
+            method: 'PATCH',
+            comment_id,
+            review_id,
+            content: comment,
+            user_id,
+          }),
+        );
+        setShowForm(false);
+      }
     }
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 w-full">
         <textarea
           {...register('content')}
           maxLength={255}
-          className="h-28 px-3 py-2 border border-darkGray rounded-2xl outline-none resize-none"
+          className="w-full px-3 py-2 border border-darkGray rounded-xl outline-none resize-none"
         />
         <div className="flex justify-between">
           <div>{errors.content?.message && <ErrorMessage message={errors.content.message} />}</div>
@@ -70,7 +69,7 @@ const CommentForm = ({ content = '', setShowForm, comment_id }: CommentFormProps
                 size="xs"
                 label="취소"
                 onClick={() => setShowForm(false)}
-                className="w-16 text-sm"
+                className="w-16 rounded-xl text-sm"
               />
             )}
             <Button
@@ -78,7 +77,7 @@ const CommentForm = ({ content = '', setShowForm, comment_id }: CommentFormProps
               color="blue"
               size="xs"
               label={!comment_id ? '등록' : '저장'}
-              className="w-16 text-sm"
+              className="w-16 rounded-xl text-sm"
             />
           </div>
         </div>
--- src/components/travel-reviews/comment/CommentItem.tsx
+++ src/components/travel-reviews/comment/CommentItem.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import LayerPopup from '@/components/common/layerPopup/LayerPopup';
-import { covertDateTime } from '@/utils/utils';
 import CommentForm from './CommentForm';
-import EditAndDelete from '../EditAndDelete';
 import { CommentItemProps } from '../types';
 import { useAppSelector } from '@/store/hooks';
 import { RootState } from '@/store/store';
-import { useDeleteCommentMutation } from '@/api/commentApi';
-import { toast } from '@/components/common/toast/Toast';
-import { DELETE_COMMENT_ERROR_MESSAGE, DELETE_COMMENT_SUCCESS_MESSAGE } from '../constants';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import { Pen, X } from 'lucide-react';
+import clsx from 'clsx';
+import { useWebSocketContext } from '@/contexts/useWebSocketContext';
 
 const CommentItem = ({
   comment: { comment_id, user_id, nickname, content, created_at },
@@ -16,39 +17,54 @@ const CommentItem = ({
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
   const [showLayerPopup, setShowLayerPopup] = useState<boolean>(false);
   const user = useAppSelector((state: RootState) => state.user.user);
-  const [deleteComment] = useDeleteCommentMutation();
+  const { sendJsonMessage, isSocketOpen } = useWebSocketContext();
+
+  dayjs.extend(relativeTime);
 
   const sendDeleteCommentRequest = () => {
-    deleteComment({ comment_id })
-      .unwrap()
-      .then(() => {
-        toast(DELETE_COMMENT_SUCCESS_MESSAGE);
-      })
-      .catch(() => {
-        toast(DELETE_COMMENT_ERROR_MESSAGE);
-      });
+    if (isSocketOpen && comment_id) {
+      sendJsonMessage(JSON.stringify({ type: 'comment', method: 'DELETE', comment_id, user_id }));
+    }
   };
 
   return (
-    <div className="flex flex-col gap-4 pb-4 border-b border-lighterGray last:border-none">
-      <div className="flex flex-col gap-1">
-        <div className="flex justify-between items-center gap-2">
-          <span className="text-darkerGray text-sm">{nickname}</span>
-          {user && user.id === user_id && (
-            <EditAndDelete
-              onClickEdit={() => setIsEditMode(true)}
-              onClickDelete={() => setShowLayerPopup(true)}
-              className="text-sm text-darkGray"
+    <div className="flex flex-col items-start gap-1 pb-2">
+      <div className="flex justify-between items-center gap-2 pl-1">
+        <span className="text-darkerGray text-sm">{nickname}</span>
+        {user && user.id === user_id && (
+          <div className="flex justify-center items-center size-3.5">
+            <Pen
+              className="shrink-0 text-darkGray cursor-pointer size-3 hover:size-3.5"
+              strokeWidth={3}
+              onClick={() => setIsEditMode(prev => !prev)}
             />
-          )}
-        </div>
-        <span className="text-darkGray text-xs">{covertDateTime(created_at)}</span>
+          </div>
+        )}
       </div>
-
-      {isEditMode && (
-        <CommentForm content={content} setShowForm={setIsEditMode} comment_id={comment_id} />
-      )}
-      {!isEditMode && <p>{content}</p>}
+      <div className="flex items-end gap-2 w-full">
+        {isEditMode && (
+          <CommentForm content={content} setShowForm={setIsEditMode} comment_id={comment_id} />
+        )}
+        {!isEditMode && (
+          <div className="flex items-start gap-2 px-3 py-2 border border-darkerGray rounded-xl bg-white">
+            {content}
+            {user && user.id === user_id && (
+              <>
+                <div className="flex justify-center items-center relative -top-0.5 -right-0.5 size-3.5">
+                  <X
+                    className="shrink-0 text-red cursor-pointer size-3 hover:size-3.5"
+                    strokeWidth={4}
+                    onClick={() => setShowLayerPopup(true)}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        )}
+        <span className={clsx('shrink-0 text-darkGray text-xs', isEditMode && 'hidden')}>
+          {dayjs().locale('ko').to(dayjs(created_at))}
+        </span>
+      </div>
 
       {showLayerPopup && (
         <LayerPopup
