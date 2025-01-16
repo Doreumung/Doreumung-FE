90c0150511389f6d868c2fee8ad4fbb97b257ed9
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-16 17:28:48 +0900

    feat: 랜딩페이지에 프리뷰 추가

--- src/components/landingPage/introduction/Introduction.tsx
+++ src/components/landingPage/introduction/Introduction.tsx
@@ -2,13 +2,15 @@ import Button from '@/components/common/buttons/Button';
 import useIsMobile from '@/hooks/useIsMobile';
 import { motion, useScroll, useTransform } from 'motion/react';
 import { useRouter } from 'next/navigation';
+import preview from '@public/images/preview.gif';
+import Image from 'next/image';
 
 const Introduction = () => {
   const isMobile = useIsMobile();
   const router = useRouter();
   const { scrollYProgress } = useScroll();
 
-  const paddingTop = useTransform(
+  const marginTop = useTransform(
     scrollYProgress,
     [0, 0.5, 1],
     [isMobile ? '112px' : '160px', '0px', '0px'],
@@ -16,11 +18,20 @@ const Introduction = () => {
 
   return (
     <motion.div
-      className="flex flex-col items-center w-full h-screen md:flex-row md:justify-between md:items-end"
-      style={{ paddingTop }}
+      className="flex flex-col justify-center items-center gap-6 relative w-full h-[cal(100% - 20px)] md:gap-10"
+      style={{ marginTop }}
     >
-      <div className="w-full h-full text-center content-center md:w-3/4">도르멍 이용 방법</div>
-      <div className="flex justify-center self-end w-full pb-20 md:w-1/4 ">
+      <div className="flex justify-center items-center relative w-full mt-20 aspect-video md:w-2/3">
+        <Image
+          src={preview}
+          fill
+          sizes="100%"
+          style={{ objectFit: 'contain' }}
+          alt="일정 생성 미리보기"
+          className="border border-darkerGray shadow-xl"
+        />
+      </div>
+      <div className="flex justify-center pb-20">
         <Button
           size={isMobile ? 'md' : 'lg'}
           color="green"
